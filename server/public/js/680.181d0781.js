63360f6a15c3f2bd49040463321475cf919ee94b
jezrelljolampong <jezrelljolampong17@gmail.com>
2024-10-28 23:22:00 -0600

    updates

--- server/public/js/680.181d0781.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self["webpackChunkclient"]=self["webpackChunkclient"]||[]).push([[680],{6680:function(n,e,t){t.r(e),t.d(e,{default:function(){return f}});var u=function(){var n=this;n._self._c;return n._m(0)},r=[function(){var n=this,e=n._self._c;return e("div",[e("h1",[n._v("Add User")])])}],l={name:"Users"},s=l,c=t(1656),i=(0,c.A)(s,u,r,!1,null,null,null),f=i.exports}}]);
-//# sourceMappingURL=680.181d0781.js.map
